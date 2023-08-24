96dccaa947835cc320459211b5cd067bf397d141
sameckmatheus <sameckmatheuspro@gmail.com>
2023-08-24 11:24:44 -0300

    Update: Interation Of JavaScript -> Events

---
+++ dist/script.js
@@ -0,0 +1,8 @@
+const GalleryElement = document.querySelector('.Gallery');
+const AddElement = document.querySelector('.Gallery-add');
+const ImgElement = document.querySelectorAll('.Gallery img');
+let newIdPhoto = 11;
+
+AddElement.addEventListener('click', () => {
+
+});
